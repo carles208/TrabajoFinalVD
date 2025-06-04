d1b39e0ffd15b4bcf6825d881404f742390cc26d
carlito208sueca <carlescarbonellsales@gmail.com>
2025-06-04 18:56:46 +0200

    Create 8_🚀_Conclusiones.py

---
+++ pages/8_🚀_Conclusiones.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("🚀 Conclusiones:")
+st.text("En conclusión, en este trabajo se ha realizado, a través del tratamiento de diversos datasets y " \
+"la creación de ciertas gráficas, un análisis de cómo importantes variables como la tasa de nacimiento, inmigración " \
+"o defunción afectan a la evolución de una población. Concretamente, se ha observado como en el caso de España, frente " \
+"al gran decremento de la natalidad y el aumento de las defunciones en la actualidad que podrían causar un gran decremento " \
+"en población, el crecimiento de la inmigración ha causado su estabilización e incluso un leve incremento. Además, también " \
+"se ha investigado como a lo largo de la evolución del país se ha invertido completamente su pirámide poblacional, pasando " \
+"de una población mayoritariamente joven a una mayor y con una muy baja natalidad (con los problemas que ello conlleva)."\
+"Finalmente, en cuanto a la distribución poblacional, se ha observado como en el caso de España, se ha presentado " \
+"el fenómeno conocido como la “España vacía”, donde a lo largo del tiempo la población ha migrado desde provincias más " \
+"rurales hacia otras con ciudades más grandes que presenten más oportunidades y un mejor nivel de vida. ")
